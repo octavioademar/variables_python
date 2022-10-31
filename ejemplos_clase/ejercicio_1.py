b6874aa910885e551108a48d96b1f5fe3579b312
octavioademar <116964211+octavioademar@users.noreply.github.com>
2022-10-31 15:00:12 -0300

    Add files via upload

---
+++ ejemplos_clase/ejercicio_1.py
@@ -0,0 +1,45 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+from functools import total_ordering
+
+
+numero_1 = 5
+numero_2 = 7
+
+# Alumno:
+# Realizar la suma de las dos variables
+numero_1 + numero_2
+# Crear una variable llamada suma donde se 
+# almacene el valor de la suma de las variable numero_1 y numero_2
+# ej:
+# suma = .....
+suma= numero_1 + numero_2
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+print(suma)
+# Repita el procedimiento para realizar la resta
+# Crear una variable llamada resta donde se 
+# almacene el valor de la resta de numero_1 menos numero_2
+numero_1 - numero_2
+resta= numero_1 - numero_2
+
+# Imprimir en pantalla el resultado de la resta
+print(resta)
+
+'''
+todo
+esto 
+es 
+un 
+comentario
+'''
